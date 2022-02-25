bc32719ee482997ed38c64e766093ec9be251aae
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2022-02-25 01:35:30 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> countingSort(List<int> arr)
+    {
+        int[] results = new int[100]; // c# by default initializes all 'int' to '0'
+        for (int i = 0; i < arr.Count; i++)
+        {
+            ++results[arr[i]];
+        }
+        return results.ToList();
+    }
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.countingSort(arr);
+
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
